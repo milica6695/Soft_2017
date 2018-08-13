4dd28b36f6d80e7a078b0a1d57765e72aa44474a
Milica Ristic <milica6695@gmail.com>
2018-08-13 20:34:18 +0200

    MALE IZMENE

--- solution.py
+++ solution.py
@@ -11,8 +11,6 @@ from keras.datasets import mnist
 def ZaDetekciju(Okvir):
     img_GRAY_gs = cv2.cvtColor(Okvir, cv2.COLOR_RGB2GRAY)  # konvert u grayscale
     # image_barcode_bin = cv2.threshold(img_GRAY_gs, 100, 200, cv2.THRESH_BINARY)   #adaptiveThreshold(img_GRAY_gs, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 61,15)
-
-    ####
     retSlike, image_bin = cv2.threshold(img_GRAY_gs, 100, 255, cv2.THRESH_OTSU)
     print("pRAG JE: " + str(retSlike))
     img, contours, hierarchy = cv2.findContours(image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -24,14 +22,13 @@ def ZaDetekciju(Okvir):
     contours_Tablica = []  # ovde ce biti samo konture koje pripadaju bar-kodu
     for contour in contours:  # za svaku konturu
         center, size, angle = cv2.minAreaRect(
-            contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
-        width, height = size
+            contour)  # proe
         xx, yy, w, h = cv2.boundingRect(contour)
         x, y = center
         if w > 3 and h > 5 and h < 400:  # uslov da kontura pripada (trebalo bi preko Krugova)
             contours_Tablica.append(contour)  # ova kontura pripada bar-kodu
-            print('Detektovano kontura(linija):  ' + str(len(contours_Tablica)))
-            print('Kordinate duzi su: ' + str(xx) + ',' + str(yy+h) + '  A druge tacke: ' + str(xx+w) +',' + str(yy)+ '  //Sirina je: ' + str(w) + ' __VISINA JE:  ' + str(h) )
+            #print('Detektovano kontura(linija):  ' + str(len(contours_Tablica)))
+            #print('Kordinate duzi su: ' + str(xx) + ',' + str(yy+h) + '  A druge tacke: ' + str(xx+w) +',' + str(yy)+ '  //Sirina je: ' + str(w) + ' __VISINA JE:  ' + str(h) )
             return xx, yy + h, xx + w, yy;
     # img = Okvir.copy()
     # cv2.drawContours(img, contours_Tablica, -1, (0, 255, 0), 1)
@@ -106,6 +103,8 @@ def napravi_model(oblik, broj_klasa=10):
 
     return model
 
+# IF NAME IS MAIN (ZA trenjiranje Neuronske mreze !!!)
+
 
 def prepoznajBroj(slika, kontura, klasifikator):
     x, y, w, h = cv2.boundingRect(kontura)
@@ -135,11 +134,9 @@ def diffImg(t0, t1, t2):  # Function to calculate difference between images.
 klasifikator = napravi_model((28,28,1),10)
 klasifikator.load_weights(''
                           'weights.h5')
-
-
 #####
 
-slika = cv2.imread("images/slika8.jpg")
+slika = cv2.imread("images/slika9.jpg")     # 8
 
 upper_red = np.array([255, 100, 100])
 lower_red = np.array([90, 0, 0])
@@ -150,7 +147,7 @@ plt.imshow(output)
 plt.show()
 
 x1, y1, x2, y2 = ZaDetekciju(output)
-print(str(x1) + ' , ' + str(y1) + '  i  ' + str(x2) + ' , ' + str(y2))
+#print(str(x1) + ' , ' + str(y1) + '  i  ' + str(x2) + ' , ' + str(y2))
 
 beleKonture = kontureBrojeva(slika)
 
@@ -166,11 +163,12 @@ for contour in contours:  # za svaku konturu
     center, size, angle = cv2.minAreaRect(contour)  # pronadji pravougaonik minimalne povrsine koji ce obuhvatiti celu konturu
     width, height = size
     xx, yy, w, h = cv2.boundingRect(contour)
-    print('xx='+ str(xx) + '  yy=0' + str(yy))
+    #print('xx='+ str(xx) + '  yy=0' + str(yy))
     #if xx >= x1 and xx <= x2 and yy <= y1 and yy >= y2:  # uslov da kontura pripada (trebalo bi preko Krugova)                                yyy:yyy + hhh, xxx:xxx + www
     #if (x1+100 >= xx >= x1 or x2+100 >= xx >= x2) and (y1+100 >= yy >= y1 or y2+100 >= yy >= y2) :  # uslov da kontura pripada (trebalo bi preko Krugova)                                yyy:yyy + hhh, xxx:xxx + www
-    if  width > 4 and height > 4:  # uslov da kontura pripada (trebalo bi preko Krugova)                                yyy:yyy + hhh, xxx:xxx + www
+    if  xx < 220 and yy < 220 and width >= 7 and height > 8:  # uslov da kontura pripada (trebalo bi preko Krugova)                                yyy:yyy + hhh, xxx:xxx + www
         cv2.imwrite('slikeBrojeva/brojevi%d.png' % index, img_GRAY[yy:yy + h, xx:xx + w])
+        print('Visina =' + str(height) + ' Sirina je: ' + str(width) )
         index = index + 1
         element = prepoznajBroj(beleKonture, contour, klasifikator)
         print( str(element) + "  !!!!!!!!!")
